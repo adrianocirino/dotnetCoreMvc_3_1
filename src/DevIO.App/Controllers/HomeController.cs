1f304e081059e912a2553fd33296e9f5a8b9001e
Adriano Cirino <adriano.cirino.da.silva@gmail.com>
2022-01-29 00:20:02 -0300

    Mapeando as anteidades em viewModels com autoMapper

--- src/DevIO.App/Controllers/HomeController.cs
+++ src/DevIO.App/Controllers/HomeController.cs
@@ -1,11 +1,7 @@
-﻿using DevIO.App.Models;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+using DevIO.App.Models;
 
 namespace DevIO.App.Controllers
 {
